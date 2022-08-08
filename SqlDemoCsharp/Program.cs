22aeea0edd7dab5027f451200b25b096a043b47e
Константин Давыдов <junecat@gmail.com>
2022-08-08 11:39:19 +0300

    Initial commit with csharp project

---
+++ SqlDemoCsharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System.IO;
+
+string fPath = Path.Combine("Logs", "Logfile.log");
+
+if (!Directory.Exists("Logs"))
+    Directory.CreateDirectory("Logs");
+
+using (StreamWriter w = File.AppendText(fPath))
+    w.WriteLine("Hello, from SQL test program...");
+
+
+const string sqlCnStr = "Password=SixStars;Persist Security Info=True;User ID=konst;Initial Catalog=TestDB;Data Source=89.179.244.127,2595;Encrypt=False";
+
+try
+{
+    using (SqlConnection conn = new SqlConnection(sqlCnStr))
+    {
+        conn.Open();
+
+        using (StreamWriter w = File.AppendText(fPath))
+            w.WriteLine("Conection opened!");
+
+        const string sqlSel = "select count(*) from Table1";
+        using (SqlCommand cmd = new SqlCommand(sqlSel, conn))
+        {
+            int rez = (int)cmd.ExecuteScalar();
+
+            using (StreamWriter w = File.AppendText(fPath))
+                w.WriteLine($"count = {rez}");
+        }
+    }
+}
+catch (Exception ex)
+{
+    using (StreamWriter w = File.AppendText(fPath))
+        w.WriteLine($"ERROR = {ex.Message}");
+}
